616b30757b3d15a07a006b2a0f6631d405f7037e
Jibril <jibrilchettaoui@gmail.com>
2023-05-26 09:12:44 +0200

    Test

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import pyxel
 
 class App:
     def __init__(self):
-        self.title = ""
+        self.title = "TITRE"
         pyxel.init(128, 128, title=self.title)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        pass
+        print(self.title)
 
     def draw(self):
         pass
